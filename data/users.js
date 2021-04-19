c25792c0c5977ceabd0ca18d240ed05f6bb0ad0e
Edward Yaroslavsky <eyarosla@stevens.edu>
2021-04-19 02:42:39 -0400

    added more db functions to user collection

--- data/users.js
+++ data/users.js
@@ -2,6 +2,11 @@ const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 let { ObjectId } = require("mongodb");
 
+async function getAllUsers() {
+    const userCollection = await users();
+    return await userCollection.find({}).toArray();
+}
+
 async function getUserById(id) {
     if (!id || typeof id !== "object") throw "Invalid ID parameter";
 
@@ -15,12 +20,11 @@ async function getUserById(id) {
 async function addUser(firstName, lastName, email, age, password, cash, stocksPurchased) {
     if (!firstName || !lastName || !email || !age || !password || !cash || !stocksPurchased)
         throw "Missing parameters";
-    if (typeof firstName !== "string" || typeof lastName !== "string" || typeof email !== "string") 
+    if (typeof firstName !== "string" || typeof lastName !== "string" || typeof email !== "string" || typeof password !== "string") 
         throw "Invalid string parameters";
     if (typeof age != "number" || age % 1 != 0) throw "Age is invalid";
     if (typeof cash != "number") throw "Cash is invalid";
     if (!Array.isArray(stocksPurchased)) throw "StocksPurchased is invalid";
-    if (typeof password !== "string" || !isHashed(password)) throw "Password is invalid";
 
     const userCollection = await users();
     let newUser = {
@@ -40,7 +44,7 @@ async function addUser(firstName, lastName, email, age, password, cash, stocksPu
     return getUserById(newInsertInformation.insertedId);
 }
 
-function isHashed(password) {
+function hashPassword(password) {
     //TODO: implement hashing for passwords
     return true;
 }
@@ -49,7 +53,55 @@ function isValidEmail(email) {
     //TODO: check if email is valid
 }
 
+async function removeUser(id) {
+    if (!id || typeof id !== "object") throw "Invalid ID parameter";
+
+    const userCollection = await users();
+    let user = null;
+    try {
+        user = await getUserById(id);
+    }
+    catch(e) {
+        console.log(e);
+        return;
+    }
+
+    const deletionInfo = await userCollection.removeOne({ _id: id });
+    if (deletionInfo.deletedCount === 0) throw "Could not delete user " + id.toString();
+
+    return true;
+}
+
+async function updateUser(id, updatedUser) {
+    if (!id || typeof id !== "object") throw "Invalid ID parameter";
+    if (!updatedUser || typeof updatedUser !== "object") throw "Invalid updatedUser parameter";
+
+    const userCollection = await users();
+    const updateUserData = {};
+
+    if (updatedUser.firstName)
+        updateUserData.firstName = updatedUser.firstName;
+    if (updatedUser.lastName)
+        updateUserData.lastName = updatedUser.lastName;
+    if (updatedUser.email)
+        updateUserData.email = updatedUser.email;
+    if (updatedUser.age)
+        updateUserData.age = updatedUser.age;
+    if (updatedUser.password)
+        updateUserData.password = updatedUser.password;
+    if (updatedUser.cash)
+        updateUserData.cash = updatedUser.cash;
+    if (updatedUser.stocksPurchased)
+        updateUserData.stocksPurchased = updatedUser.stocksPurchased;
+
+    await userCollection.updateOne({ _id: id }, { $set: updateUserData });
+
+    return await getUserById(id);
+}
+
 module.exports = {
     getUserById,
-    addUser
+    addUser,
+    removeUser,
+    updateUser
 };
