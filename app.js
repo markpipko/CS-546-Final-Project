1f8087a7bac3219bcec78755b220e9c12efa13b6
Edward Yaroslavsky <eyarosla@stevens.edu>
2021-04-19 22:09:03 -0400

    Added initial login page and logic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const configRoutes = require('./routes');
 const static = express.static(__dirname + "/public");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 
 app.use("/public", static);
 app.use(express.json());
@@ -10,6 +11,37 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(
+    session({
+        name: "PaperTradingApp",
+        secret: "This is a secret",
+        saveUnitialized: true,
+        resave: false,
+        cookie: { maxAge: 60000 }
+    })
+);
+
+app.use("/private", (req, res, next) => {
+    console.log(req.session.id);
+    if (!req.session.user) {
+        return res.redirect("/");
+    }
+    else {
+        next();
+    }
+});
+
+app.use("/login", (req, res, next) => {
+    if (req.session.user) {
+        return res.redirect("/private");
+    }
+    else {
+        req.method = "POST";
+        next();
+    }
+});
+
 configRoutes(app);
 
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const stocksRoutes = require("./stocks");
+const privateRoutes = require("./private");
+const userRoutes = require("./users");
 
 const constructorMethods = (app) => {
-	app.get("/", (req, res) => {
+	app.use("/", userRoutes);
+	app.use("/private", privateRoutes);
+
+	//No need for this since these are under the private route now
+	/*app.get("/home", (req, res) => {
 		res.render("home", { title: "Home" });
 	});
-	app.use("/stocks", stocksRoutes);
+	app.use("/stocks", stocksRoutes);*/
+
 	app.use("*", (req, res) => {
 		res.sendStatus(404);
 	});
