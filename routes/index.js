fb9ac7e8c5c9efc9be3f5d2271129d19c4d09720
jnelson6 <jnelson6@stevens.edu>
2021-04-25 16:58:42 -0400

    Update index.js
    unfinished
    added  buy/sell path
    added stockhistory (to view) path

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const stocksRoutes = require("./stocks");
 
+const buyRoutes = require("./buysell"); //import the file buysRoute
+const historyRoutes = require("./stockHistory");
+
 const constructorMethods = (app) => {
 	app.get("/", (req, res) => {
 		res.render("home", { title: "Home" });
 	});
+
+	//buy/sell form path here
+	app.get("/buysell", (req, res) => {
+		res.render("buysell", { title: "Buy/Sell" });
+	});
+
+	//history form path here (handlebar)
+	app.get("/stockHistory", (req, res) => {
+		res.render("stockHistory", { title: "History" });
+	});
+
 	app.use("/stocks", stocksRoutes);
+
+	
+	app.use("/buysell",  buyRoutes);
+	//add stockhistory route linked to stockHistory.js file
+	app.use("/stockHistory", historyRoutes);
+
 	app.use("*", (req, res) => {
 		res.sendStatus(404);
 	});
