c9d45a7dc588c6ea3188c6d771f065e280b13213
Christopher Moon <cmoon1@stevens.edu>
2021-05-01 22:27:06 -0400

    Added success message

--- public/js/stock.js
+++ public/js/stock.js
@@ -10,7 +10,7 @@ const stockError = document.getElementById("error_container2");
 const refreshForm = document.getElementById("refresh");
 const temp = document.getElementById("temp_ticker");
 
-const yahooStockPrices = require("yahoo-stock-prices");
+// const yahooStockPrices = require("yahoo-stock-prices");
 
 // if (myForm) {
 // 	myForm.addEventListener("submit", (event) => {
@@ -221,50 +221,59 @@ if (transForm) {
 					quantity: quantity,
 				}),
 			}).then(function (x) {
+				let result = document.createElement("p");
+				result.innerHTML = `Your order to ${transaction} ${quantity} share of ${temp.value.toUpperCase()} was successful.`;
+				$("#dialog-message").append(result);
+				$("#dialog-message").dialog({
+					modal: true,
+					buttons: {
+						Ok: function () {
+							$(this).dialog("close");
+						},
+					},
+				});
 				transForm.reset();
-				console.log("sucess???");
 				$.unblockUI();
 			});
 		}
 	});
 }
 
-$('#1w, #1m, #1y, #5y').click( function(event) {
-	event.preventDefault();
-	var num
-	if($(event.target).attr('id')=='1w'){
-		num = 7
-	} else if($(event.target).attr('id')=='1m'){
-		num = 30
-	}else if($(event.target).attr('id')=='1y'){
-		num = 365
-	}else if($(event.target).attr('id')=='5y'){
-		num = 365*5
-	}
-	var prices = [] //y-axis
-	var dates = [] //x-axis
-	var today = new Date()
-	var monthAgo
-	monthAgo.setDate(today.getDate() - num)
-	const stocksData = await yahooStockPrices.getHistoricalPrices(
-		monthAgo.getMonth(),
-		monthAgo.getDate(),
-		monthAgo.getFullYear(), 
-		today.getMonth(),
-		today.getDate(),
-		today.getFullYear(), 
-		input, 
-		'1d');
-	for(var i = 0; i < stocksData.length; i++){
-		prices[i] = stocksData[i].adjclose
-		dates[i] = `${monthAgo.getMonth()+1}/${monthAgo.getDate()}/${monthAgo.getFullYear()}`
-	}
-	var trace = {
-		x: dates,
-		y: prices,
-		mode: 'lines'
-	}
+// $('#1w, #1m, #1y, #5y').click( function(event) {
+// 	event.preventDefault();
+// 	var num;
+// 	if($(event.target).attr('id')=='1w'){
+// 		num = 7
+// 	} else if($(event.target).attr('id')=='1m'){
+// 		num = 30
+// 	}else if($(event.target).attr('id')=='1y'){
+// 		num = 365
+// 	}else if($(event.target).attr('id')=='5y'){
+// 		num = 365*5
+// 	}
+// 	var prices = [] //y-axis
+// 	var dates = [] //x-axis
+// 	var today = new Date()
+// 	var monthAgo
+// 	monthAgo.setDate(today.getDate() - num)
+// 	const stocksData = await yahooStockPrices.getHistoricalPrices(
+// 		monthAgo.getMonth(),
+// 		monthAgo.getDate(),
+// 		monthAgo.getFullYear(),
+// 		today.getMonth(),
+// 		today.getDate(),
+// 		today.getFullYear(),
+// 		input,
+// 		'1d');
+// 	for(var i = 0; i < stocksData.length; i++){
+// 		prices[i] = stocksData[i].adjclose
+// 		dates[i] = `${monthAgo.getMonth()+1}/${monthAgo.getDate()}/${monthAgo.getFullYear()}`
+// 	}
+// 	var trace = {
+// 		x: dates,
+// 		y: prices,
+// 		mode: 'lines'
+// 	}
 
-	Ploty.newPlot('graph', trace)
-})
-
+// 	Ploty.newPlot('graph', trace)
+// })
