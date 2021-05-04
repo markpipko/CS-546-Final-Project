8824f0598b294d925e9e4d63b07adcfe4e1b739c
Edward Yaroslavsky <eyarosla@stevens.edu>
2021-05-04 18:00:52 -0400

    seeding, bug fixes, improvements

--- public/js/home.js
+++ public/js/home.js
@@ -6,7 +6,7 @@ const refresh = document.getElementById("refresh");
 
 var pValue = []
 var dates = []
-
+var repititions = 0;
 
 refresh.click(function (event) {
 	event.preventDefault();
@@ -23,20 +23,34 @@ refresh.click(function (event) {
 });
 
 $(document).ready(function() {
-	var myTime = setInterval(graph, 5000) //calls every 5 seconds
-	graph()
+	var myTime = setInterval(graph, 5000); //calls every 5 seconds
+	graph();
 })
 
 function graph(){
+	if (repititions >= 5) {
+		var currTime = new Date();
+		var currHour = currTime.getHours();
+		var currMin = currTime.getMinutes();
+
+		if (currHour < 9 || currHour > 4 || (currHour == 9 && currMin < 30)) {
+			//TODO: Can set Dark Mode for closed hours here
+			return;
+		}
+
+		repititions = 0;
+	}
+	repititions++;
+
 	var requestConfig = {
 		method: "POST",
 		url: `/private/userGraph`,
 	};
 
 	$.ajax(requestConfig).then(function (responseMessage) {
-		pValue.push(responseMessage.value)
-		var today = new Date()
-		dates.push(today)
+		pValue.push(responseMessage.value);
+		var today = new Date();
+		dates.push(today);
 
 		var trace = {
 			x: dates,
@@ -45,7 +59,7 @@ function graph(){
 		};
 
 		var data = [trace];
-		Plotly.newPlot("userGraph", data)
+		Plotly.newPlot("userGraph", data);
 	})
 }
 
